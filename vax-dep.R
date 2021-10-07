cc849013cb03263e6a2bbe53cbd8fa1cd17db28e
Aaron Schiff <aaron@schiff.nz>
2021-10-07 20:24:01 +1300

    added sa2 level data to vaccination vs deprivation comparison

--- vax-dep.R
+++ vax-dep.R
@@ -1,4 +1,4 @@
-# Vaccination rates vs deprivation index by TLA area
+# Vaccination rates vs deprivation index
 
 
 # *****************************************************************************
@@ -12,8 +12,8 @@ library(glue)
 library(scales)
 library(ggrepel)
 
-latest_date <- "05_10_2021"               # Date of most recent week's data
-latest_date_nice <- "5 October 2021"   # For chart title
+latest_date_tla <- "05_10_2021"  # Date of most recent week's TLA data
+latest_date_sa2 <- "20211006"    # Date of most recent week's SA2 data
 
 # *****************************************************************************
 
@@ -22,11 +22,19 @@ latest_date_nice <- "5 October 2021"   # For chart title
 # Load data ----
 
 # Vaccination data by TLA
-dat_vax <- read_csv(file = here(glue("data/vaccination_tla_{latest_date}.csv"))) |> 
+dat_vax_tla <- read_csv(file = here(glue("data/vaccination_tla_{latest_date_tla}.csv"))) |> 
   clean_names() |> 
   select(-percent_had_first_dose, -percent_fully_vaccinated) |> 
   mutate(first_dose_rate = first_dose_administered / population, 
          fully_vax_rate = second_dose_administered / population)
+
+# Vaccination data by SA2
+dat_vax_sa2 <- read_csv(file = here(glue("data/uptake_sa2_dhb_{latest_date_sa2}.csv"))) |> 
+  mutate(dose1_cnt = as.integer(dose1_cnt), 
+         dose2_cnt = as.integer(dose2_cnt), 
+         pop_cnt = as.integer(pop_cnt), 
+         dose1_uptake = as.integer(dose1_uptake), 
+         dose2_uptake = as.integer(dose2_uptake))
 
 # Deprivation index data
 dat_dep <- read_excel(path = here("data/otago823836.xlsx")) |> 
@@ -36,7 +44,7 @@ dat_dep <- read_excel(path = here("data/otago823836.xlsx")) |>
 
 
 # *****************************************************************************
-# Compare deprivation and vaccination ----
+# Compare deprivation and vaccination by TLA ----
 
 # Deprivation vs vaccination data
 dat_dep_vax_tla <- 
@@ -50,7 +58,7 @@ dat_dep_vax_tla <-
   filter(ta_2018_name != "Area Outside Territorial Authority", 
          ta_2018_name != "Chatham Islands Territory") |> 
   # Join to vaccination data
-  left_join(y = dat_vax, by = c("ta_2018_name" = "tla")) |> 
+  left_join(y = dat_vax_tla, by = c("ta_2018_name" = "tla")) |> 
   arrange(desc(population)) |> 
   # Create shortened TLA labels
   mutate(ta_name_short = str_remove(string = ta_2018_name, pattern = "City")) |> 
@@ -94,6 +102,60 @@ chart_dat_dep_vax_tla <- dat_dep_vax_tla |>
 
 ggsave(filename = here("outputs/vax_vs_dep_tla.png"), 
        plot = chart_dat_dep_vax_tla, 
+       device = "png", 
+       width = 2000, 
+       height = 2000, 
+       units = "px", 
+       bg = "white")
+
+# *****************************************************************************
+
+
+# *****************************************************************************
+# Compare deprivation and vaccination by SA2 ----
+
+# Deprivation vs vaccination data
+dat_dep_vax_sa2 <- 
+  # SA2-level population-weighted average deprivation score
+  dat_dep |> 
+  group_by(sa22018_code, sa22018_name, ta_2018_name) |> 
+  summarise(wt_avg_nz_dep2018_score = weighted.mean(x = nz_dep2018_score, 
+                                                    w = ur_popn_sa1_2018, 
+                                                    na.rm = TRUE)) |> 
+  ungroup() |> 
+  filter(ta_2018_name != "Area Outside Territorial Authority", 
+         ta_2018_name != "Chatham Islands Territory") |> 
+  # Join to vaccination data
+  left_join(y = dat_vax_sa2, by = c("sa22018_code" = "sa2_code"))
+
+chart_dat_dep_vax_sa2 <- dat_dep_vax_sa2 |> 
+  ggplot(mapping = aes(x = wt_avg_nz_dep2018_score, 
+                       y = dose1_uptake / 1000)) + 
+  geom_point(colour = "cornflowerblue", size = 0.5) + 
+  scale_y_continuous(limits = c(0.3, 1),
+                     breaks = seq(0.3, 1, .05),
+                     labels = percent_format(accuracy = 1),
+                     expand = expansion(0, 0)) +
+  scale_x_continuous(limits = c(800, 1400),
+                     breaks = seq(800, 1400, 100),
+                     labels = comma_format(accuracy = 1)) +
+  theme_minimal(base_family = "Fira Sans") + 
+  xlab("Population-weighted average deprivation score") + 
+  ylab("First dose\nvaccination rate") + 
+  theme(panel.grid.minor = element_blank(), 
+        plot.margin = margin(8, 16, 8, 8, "pt"), 
+        plot.title = element_text(size = rel(1.1), 
+                                  face = "bold", 
+                                  margin = margin(0, 0, 8, 0, "pt")), 
+        axis.title.y = element_text(angle = 0, 
+                                    margin = margin(0, 8, 0, 0, "pt"), 
+                                    hjust = 0, 
+                                    face = "bold"), 
+        axis.title.x = element_text(face = "bold", 
+                                    margin = margin(8, 0, 0, 0, "pt")))
+
+ggsave(filename = here("outputs/vax_vs_dep_sa2.png"), 
+       plot = chart_dat_dep_vax_sa2, 
        device = "png", 
        width = 2000, 
        height = 2000, 
